938bb26cb634bf9151331ea84f3d6ce85136d97b
ShreyShukla14001 <92687484+ShreyShukla14001@users.noreply.github.com>
2023-11-09 14:47:23 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import setup as set
+import sol
+import sol as s
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    input_str=input("Enter input string")
+    df=set.df
+    print(sol.find_closest_match(input_str,df))
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
