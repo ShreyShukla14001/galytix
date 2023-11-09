b16d5acb1dc9d990ec2ea4c1f5de89e65aaf19d4
ShreyShukla14001 <92687484+ShreyShukla14001@users.noreply.github.com>
2023-11-09 14:47:47 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import gensim
+from gensim.models import KeyedVectors
+import sol
+df = pd.read_csv('C:/Users/shrey/Downloads/DE assessment/phrases (1).csv',encoding='unicode_escape')
+pth="C:/Users/shrey/Downloads/archive (4)/GoogleNews-vectors-negative300.bin.gz"
+wv=KeyedVectors.load_word2vec_format(pth,binary=True,limit=1000000)
+wv.save_word2vec_format('vectors.csv')
+
+
+
