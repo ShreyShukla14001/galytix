27e081d324bed71c7d369dc98a8ac4f48131be2e
ShreyShukla14001 <92687484+ShreyShukla14001@users.noreply.github.com>
2023-11-09 15:04:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import setup as set
-import sol
 import sol as s
 
 def print_hi(name):
