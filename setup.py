b6ad38b90933ee8e3dca68741bc3c590cd67ae9b
ShreyShukla14001 <92687484+ShreyShukla14001@users.noreply.github.com>
2023-11-09 15:04:41 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import gensim
 from gensim.models import KeyedVectors
-import sol
 df = pd.read_csv('C:/Users/shrey/Downloads/DE assessment/phrases (1).csv',encoding='unicode_escape')
 pth="C:/Users/shrey/Downloads/archive (4)/GoogleNews-vectors-negative300.bin.gz"
 wv=KeyedVectors.load_word2vec_format(pth,binary=True,limit=1000000)
